2d22c6944a9b31f7548fb73809b082702678873f
LoGinShin <95666408+loginshin@users.noreply.github.com>
2026-03-22 20:54:41 +0900

    modify(sql) : boards sql 수정

--- DB/boards.sql
+++ DB/boards.sql
@@ -5,18 +5,18 @@ DROP TABLE IF EXISTS boards CASCADE;
 
 CREATE TABLE boards
 (
-    board_id VARCHAR(36) NOT NULL,
+    board_key BIGSERIAL NOT NULL,
     board_title VARCHAR(150) NOT NULL,
     board_contents TEXT,
-    create_user_key VARCHAR(36),
+    create_user_key VARCHAR(128),
     create_dt TIMESTAMP,
-    update_user_key VARCHAR(36),
+    update_user_key VARCHAR(128),
     update_dt TIMESTAMP,
-    CONSTRAINT boards_pk PRIMARY KEY (board_id)
+    CONSTRAINT boards_pk PRIMARY KEY (board_key)
 );
 
 COMMENT ON TABLE boards IS '게시판';
-COMMENT ON COLUMN boards.board_id IS '게시판 번호';
+COMMENT ON COLUMN boards.board_key IS '게시판 키(순차)';
 COMMENT ON COLUMN boards.board_title IS '게시판 제목';
 COMMENT ON COLUMN boards.board_contents IS '게시판 내용';
 COMMENT ON COLUMN boards.create_user_key IS '등록자';
